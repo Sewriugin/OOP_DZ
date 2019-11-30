c65ff40ea9fdcbc24019622bcd394268694d46c8
Admin <alex.star-2011@yandex.ru>
2019-11-30 22:08:48 +0400

    DZ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,35 @@
 package com.company;
                                       // Практика - Business
-//public class Main {
-//    public static void main(String[] args) {
-//	// write your code here
-//        Business business1 = new Business();
-//        Business business2 = new Business();
-//
-//        String [] massivphotostudio = {"bussines1 ", "bussines2 "};
-//        for (String result1: massivphotostudio) {
-//            System.out.print(result1);
-//        }
-//        System.out.println();
-//        String [] massivKeyCutting = {"bussines1 ", "bussines2 "};
-//        for (String result2: massivKeyCutting) {
-//            System.out.print(result2);
-//        }
-//        System.out.println();
-//
-//        business1.addToYandexMap();
-//        business1.findKeyMaster();
-//        business1.getLicense();
-//        business1.makeSignboard();
-//        business1.KeyCutting();
-//
-//        business2.addToYandexMap();
-//        business2.findKeyMaster();
-//        business2.getLicense();
-//        business2.makeSignboard();
-//        business2.KeyCutting();
-//    }
-//}
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        Business business1 = new Business();
+        Business business2 = new Business();
+
+        String [] massivphotostudio = {"bussines1 ", "bussines2 "};
+        for (String result1: massivphotostudio) {
+            System.out.print(result1);
+        }
+        System.out.println();
+        String [] massivKeyCutting = {"bussines1 ", "bussines2 "};
+        for (String result2: massivKeyCutting) {
+            System.out.print(result2);
+        }
+        System.out.println();
+
+        business1.addToYandexMap();
+        business1.findKeyMaster();
+        business1.getLicense();
+        business1.makeSignboard();
+        business1.KeyCutting();
+
+        business2.addToYandexMap();
+        business2.findKeyMaster();
+        business2.getLicense();
+        business2.makeSignboard();
+        business2.KeyCutting();
+    }
+}
                             // Практика - Football
 //public class Main {
 //    public static void main(String[] args) {
@@ -57,15 +57,27 @@ package com.company;
 //}
 
                         // Практика - Деревья
-public class Main {
-    public static void main(String[] args) {
-        // write your code here
+//public class Main {
+//    public static void main(String[] args) {
+//        // write your code here
+//
+//       String [] trees = {"AppleTree ", "BirchTree ", "ChristmasTree "};
+//        for (String result: trees) {
+//            System.out.print(result.toString());
+//        }
+//
+//    }
+//}
 
-       String [] trees = {"AppleTree ", "BirchTree ", "ChristmasTree "};
-        for (String result: trees) {
-            System.out.print(result.toString());
-        }
-
-    }
-}
-
+                       // Практика - Repaii
+//public class Main {
+//    public static void main(String[] args) {
+//        // write your code here
+//
+//        String [] trees = {"AppleTree ", "BirchTree ", "ChristmasTree "};
+//        for (String result: trees) {
+//            System.out.print(result.toString());
+//        }
+//
+//    }
+//}
