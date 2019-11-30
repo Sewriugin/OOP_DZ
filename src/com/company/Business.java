8ec76a6cf3db152ba581dc74068b8455584ae1e1
Admin <alex.star-2011@yandex.ru>
2019-11-30 22:08:25 +0400

    DZ

--- src/com/company/Business.java
+++ src/com/company/Business.java
@@ -1,13 +1,29 @@
 package com.company;
 
 public class Business implements Photostudio, KyeCutting {
+
+    @Override
+    public void getLicense() {
+        System.out.println("Добавляю на Яндекс карты_1");
+    }
+
+    @Override
+    public void makeSignboard() {
+        System.out.println("Добавляю на Яндекс карты_2");
+    }
+
+    @Override
+    public void findKeyMaster() {
+        System.out.println("Добавляю на Яндекс карты_3");
+    }
+
     public void addToYandexMap() {
         System.out.println("Photostudio");
     }
     public void KeyCutting() {
         System.out.println("KeyCutting");
     }
-
-    public Business() {
-    }
+//
+//    public Business() {
+//    }
 }
