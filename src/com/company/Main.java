befac0d8deb178f257aa8f75142e8be6dec869e6
Admin <alex.star-2011@yandex.ru>
2019-11-30 22:47:52 +0400

    DZ

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,40 +1,41 @@
 package com.company;
                                       // Практика - Business
-public class Main {
-    public static void main(String[] args) {
-	// write your code here
-        Business business1 = new Business();
-        Business business2 = new Business();
-
-        String [] massivphotostudio = {"bussines1 ", "bussines2 "};
-        for (String result1: massivphotostudio) {
-            System.out.print(result1);
-        }
-        System.out.println();
-        String [] massivKeyCutting = {"bussines1 ", "bussines2 "};
-        for (String result2: massivKeyCutting) {
-            System.out.print(result2);
-        }
-        System.out.println();
-
-        business1.addToYandexMap();
-        business1.findKeyMaster();
-        business1.getLicense();
-        business1.makeSignboard();
-        business1.KeyCutting();
-
-        business2.addToYandexMap();
-        business2.findKeyMaster();
-        business2.getLicense();
-        business2.makeSignboard();
-        business2.KeyCutting();
-    }
-}
+//public class Main {
+//    public static void main(String[] args) {
+//	// write your code here
+//        Business business1 = new Business();
+//        Business business2 = new Business();
+//
+//        String [] massivphotostudio = {"bussines1 ", "bussines2 "};
+//        for (String result1: massivphotostudio) {
+//            System.out.print(result1);
+//        }
+//        System.out.println();
+//        String [] massivKeyCutting = {"bussines1 ", "bussines2 "};
+//        for (String result2: massivKeyCutting) {
+//            System.out.print(result2);
+//        }
+//        System.out.println();
+//
+//        business1.addToYandexMap();
+//        business1.findKeyMaster();
+//        business1.getLicense();
+//        business1.makeSignboard();
+//        business1.KeyCutting();
+//
+//        business2.addToYandexMap();
+//        business2.findKeyMaster();
+//        business2.getLicense();
+//        business2.makeSignboard();
+//        business2.KeyCutting();
+//    }
+//}
                             // Практика - Football
 //public class Main {
 //    public static void main(String[] args) {
 //        // write your code here
 //        Player player = new Player();
+//
 //        player.scoreVeryRarely();
 //        player.scoreRarely();
 //        player.scoreOften();
